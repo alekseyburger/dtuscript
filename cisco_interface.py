ae22b9794ea922976a7c92d2f2ebddec338f1a1e
Aleksey Burger <aleksey.burger@exaware.com>
2025-02-03 14:44:32 +0200

    Improve CiscoInterface class

--- cisco_interface.py
+++ cisco_interface.py
@@ -9,7 +9,6 @@ from exception_dev import ExceptionDevice
 import utils_ipv4
 from cisco_vrf import CiscoVrf
 
-
 logger = logging.getLogger("dtulibLog")
 def trace(format):
     logger.debug(f'\n\x1b[1;32mCiscoInterface: {format}\x1b[0m')
@@ -27,32 +26,32 @@ def get_vrf_name (vrf_or_name):
 
 PROMPT_CFG = '(config-if)#'
 class CiscoInterface(BaseConfig):
+    '''
+    The CiscoInterface object contains the desired configuration for the interface.
+    However, the object is created not attached to the interface. Changing the 
+    attributes of an non-attached object does not cause changes in the router 
+    configuration until the .attach or .create methods are called. The .create 
+    method creates an interface with the default configuration on the router. 
+    The .attach method makes the object connected to the interface, but does not 
+    change the interface configuration. After calling these methods, it is possible 
+    to change the interface configuration using the .modify method or delete/clear 
+    the interface using the .delete method.
+    '''
+
+    attr_list = ("vrf", "ipv4_address_mask", "ipv6_address_mask","description", "mpls",
+        "vlanId")
+
     def __init__ (self, name):
         BaseConfig.__init__(self, None, name.lower())
 
         self.is_subinterface = (1 == len(re.findall(r"([a-zA-Z\-]+)([0-9\/]+)\.(\d+)",self.name)))
         self.is_loopback = (1 == len(re.findall(r"(loopback)\d+",self.name)))
-        # self.__args_parse__(**kwargs)
-
-    # def __args_parse__(self, **kwargs):
-    #     for feature in kwargs.keys():
-    #         if feature == "vrf":
-    #             self.vrf = kwargs[feature]
-    #         elif feature == "ipv4_address_mask":
-    #             self.ipv4_address_mask = kwargs[feature]
-    #         elif feature == "ipv6_address_mask":
-    #             self.ipv6_address_mask = kwargs[feature]
-    #         elif feature == "description":
-    #             self.description = kwargs[feature]
-    #         elif feature == "mpls":
-    #             self.mpls = kwargs[feature]
-    #         elif feature == "vlanId":
-    #             self.vlanId = kwargs[feature]
-    #         else:
-    #             error(f" Unexpected cfg feature {feature}")
 
     @property
     def ipv4_address (self):
+        '''
+        This method returns the IP address assigned to interface
+        '''
         if not hasattr(self, "ipv4_address_mask") or self.ipv4_address_mask is None:
             return None
         match = re.findall(r'(\d+.\d+.\d+.\d+)\/\d+',self.ipv4_address_mask)
@@ -62,6 +61,9 @@ class CiscoInterface(BaseConfig):
     
     @property
     def ipv4_mask (self):
+        '''
+        This method returns the IP network mask assigned to interface 
+        '''
         if not hasattr(self, "ipv4_address_mask") or self.ipv4_address_mask is None:
             return None
         match = re.findall(r'\d+.\d+.\d+.\d+\/(\d+)',self.ipv4_address_mask)
@@ -83,35 +85,11 @@ class CiscoInterface(BaseConfig):
             ret = ret + " (LoopBack) "
         return ret
 
-    # def __apply__ (self):
-    #     if hasattr(self, "vrf") and not self.vrf is None:
-    #         vrf_name = get_vrf_name(self.vrf)
-    #         if vrf_name and len(vrf_name):
-    #             self.router.writeWithResponce(f"ip vrf forwarding {vrf_name}",PROMPT_CFG)
-
-    #     if hasattr(self, "ipv4_address_mask") and not self.ipv4_address_mask is None:
-    #         self.router.writeWithResponce(f"ip address  {self.ipv4_address} {self.ipv4_mask}",
-    #             PROMPT_CFG)
-    #     else:
-    #         self.router.writeWithResponce(f"no ip address",PROMPT_CFG)
-    #     if hasattr(self, "ipv6_address_mask") and not self.ipv6_address_mask is None:
-    #         self.router.writeWithResponce('ipv6 enable',PROMPT_CFG)
-    #         self.router.writeWithResponce(f"ipv6 address {self.ipv6_address_mask}",PROMPT_CFG)
-    #     else:
-    #         self.router.writeWithResponce(f"no ipv6 address",PROMPT_CFG)
-    #         self.router.writeWithResponce('no ipv6 enable',PROMPT_CFG)
-    #     if hasattr(self, "description"):
-    #         self.router.writeWithResponce(f'description "{self.description}"',PROMPT_CFG)
-    #     if not self.is_loopback:
-    #         if hasattr(self, "mpls") and self.mpls:
-    #             self.router.writeWithResponce(f"mpls ip",PROMPT_CFG)
-    #         else:
-    #             self.router.writeWithResponce(f"no mpls ip",PROMPT_CFG)
-    #     if not self.is_loopback:
-    #         if hasattr(self, "vlanId") and self.vlanId:
-    #             self.router.writeWithResponce(f"vlan-id dot1q {self.vlanId}",PROMPT_CFG)
-    #         # else:
-    #         #     self.router.writeWithResponce(f"no vlan-id dot1q",PROMPT_CFG)
+    def __set_feature__(self, feature, value):
+        if feature not in self.attr_list:
+            error(f" Unexpected cfg feature {feature}")
+            return
+        setattr(self, feature, value) 
 
     def __apply_feature__(self, feature, value):
         if feature == "vrf":
@@ -169,38 +147,72 @@ class CiscoInterface(BaseConfig):
         else:
             error(f" Unexpected cfg feature {feature}")
 
+    def __apply_features__(self):
+        for feature in self.attr_list:
+            if hasattr(self, feature):
+                self.__apply_feature__(feature, getattr(self, feature))
+
     def attach (self, router):
-        router.toExec()
+        '''
+        If interface is exist, then attach the Object to router interface and returns True.
+        Otherwise retunts False.  After calling these methods, it is possible 
+        to change the interface configuration using the .modify method or delete/clear 
+        the interface using the .delete method.
+        '''
         int_list = cisco_get_all_interfaces(router)
         if self.name not in int_list:
             return False
-        self.router = router        
+        self.router = router
         return True
 
     
     def create (self, router):
+        '''
+        Create interface with default configuration. After calling these methods, 
+        it is possible     to change the interface configuration using the .modify 
+        method or delete/clear the interface using the .delete method.
+        '''
+        int_list = cisco_get_all_interfaces(router)
         self.router = router
-
+        # clean interface configuration
         self.router.toConfig()
         if not self.is_subinterface and not self.is_loopback:
             self.router.writeWithResponce(f"default interface {self.name}",'(config)#')
+        elif self.name in int_list:
+            self.router.writeWithResponce(f"no interface {self.name}",'(config)#')
+        # recreate interface and applay configured features
         self.router.writeWithResponce(f"interface {self.name}",PROMPT_CFG)
-        # self.__apply__()
+        self.__apply_features__()
         info(f" {self} created")
         return True
 
-    def modify (self, router=None, **kwargs):
-        if not self.router and router:
-            self.router = router
+    def modify (self, **kwargs):
+        '''
+        This method has different effects for attached and unattached objects. 
+        If the object is not attached to an interface, the .modify method simply 
+        stores the desired configuration attributes in the object. This 
+        configuration will be applied to the interface when the .create 
+        method is called. If the object is attached to an interface, then 
+        calling the method affects the interface configuration immediately.
+        '''
 
         if self.router:
             self.router.toConfig()
             self.router.writeWithResponce(f"interface {self.name}",PROMPT_CFG)
             for feature, value in kwargs.items():
                 self.__apply_feature__(feature, value)
+        else:
+            for feature, value in kwargs.items():
+                self.__set_feature__(feature, value)
         info(f" {self} modified")
 
+
     def delete (self, router=None):
+        '''
+        This method removes the interface from the configuration if it is a 
+        loopback or subinterface. It applies the default configuration to 
+        the physical interface.
+        '''
         if router:
             self.router = router
 
@@ -214,16 +226,22 @@ class CiscoInterface(BaseConfig):
         self.router = None
 
     def up (self):
+        '''
+        This method apply up state to interface. It is expected that object
+        is attached to interface
+        '''
         self.router.toConfig()
-
         self.router.writeWithResponce(f"interface {self.name}",PROMPT_CFG)
         self.router.writeWithResponce(f"no shutdown",PROMPT_CFG)      
 
         info(f" {self} up")
 
     def down (self):
+        '''
+        This method apply down state to interface. It is expected that object
+        is attached to interface
+        '''
         self.router.toConfig()
-
         self.router.writeWithResponce(f"interface {self.name}",PROMPT_CFG)
         self.router.writeWithResponce(f"shutdown",PROMPT_CFG)      
 
@@ -231,12 +249,14 @@ class CiscoInterface(BaseConfig):
 
 
 def cisco_get_all_interfaces (router):
+    '''
+    Returns the list of existing interfaces (from 'show ip interface brief')
+    '''
     router.toExec()
     router.writeWithResponce('show ip interface brief')
     int_list = []
 
     #remove show header
-    # delimiter_pos = re.search(r'Interface\s+IP-Address.+(Protocol$)', router.resp, re.MULTILINE)
     delimiter_pos = re.search(r'Interface\s+IP-Address.+(Protocol\r\n)', router.resp, re.MULTILINE)
     if not delimiter_pos:
         return int_list
